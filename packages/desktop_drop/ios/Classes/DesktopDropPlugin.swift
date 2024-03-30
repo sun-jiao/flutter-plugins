9efc79bd67eebf5e3298c9a88b7962f78a7ebfff
孙娇 <P.ozzy@outlook.com>
2024-03-30 10:51:41 +0800

    work on ios drop

--- packages/desktop_drop/ios/Classes/DesktopDropPlugin.swift
+++ packages/desktop_drop/ios/Classes/DesktopDropPlugin.swift
@@ -1,27 +1,55 @@
 import Flutter
 import UIKit
+import MobileCoreServices
 
-public class DesktopDropPlugin: NSObject {
+public class DesktopDropPlugin: NSObject, FlutterPlugin {
     private var channel: FlutterMethodChannel?
     private weak var view: UIView?
-    private weak var viewController: UIViewController?
 
-    public init(with registrar: FlutterPluginRegistrar) {
+    // Initializer is now private to enforce factory method usage
+    private override init() {
         super.init()
-        channel = FlutterMethodChannel(name: "desktop_drop", binaryMessenger: registrar.messenger())
-        registrar.addMethodCallDelegate(self, channel: channel!)
     }
 
-    public func setView(_ view: UIView?, viewController: UIViewController?) {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "desktop_drop", binaryMessenger: registrar.messenger())
+        let instance = DesktopDropPlugin()
+        instance.channel = channel
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        // Assuming the view to add drag interaction is the registrar's view
+//         if let view = registrar.view() {
+//             instance.setView(view)
+//         }
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "enableDrop":
+            setupDragInteraction()
+            result(nil)
+        case "disableDrop":
+            removeDragInteraction()
+            result(nil)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func setView(_ view: UIView) {
         self.view = view
-        self.viewController = viewController
-        setupDragInteraction()
     }
 
     private func setupDragInteraction() {
         guard let view = view else { return }
         let dropInteraction = UIDropInteraction(delegate: self)
         view.addInteraction(dropInteraction)
+    }
+
+    private func removeDragInteraction() {
+        guard let view = view else { return }
+        if let interaction = view.interactions.first(where: { $0 is UIDropInteraction }) {
+            view.removeInteraction(interaction)
+        }
     }
 
     private func handleDrop(_ session: UIDropSession) {
@@ -35,16 +63,6 @@ public class DesktopDropPlugin: NSObject {
     }
 }
 
-extension DesktopDropPlugin: FlutterPlugin {
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let instance = DesktopDropPlugin(with: registrar)
-    }
-
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(FlutterMethodNotImplemented)
-    }
-}
-
 extension DesktopDropPlugin: UIDropInteractionDelegate {
     public func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
         return session.hasItemsConforming(toTypeIdentifiers: [kUTTypeFileURL as String])
@@ -52,18 +70,18 @@ extension DesktopDropPlugin: UIDropInteractionDelegate {
 
     // Handle drag enter event
     public func dropInteraction(_ interaction: UIDropInteraction, sessionDidEnter session: UIDropSession) {
-        channel.invokeMethod("entered", arguments: nil)
+        channel?.invokeMethod("entered", arguments: nil)
     }
 
     // Handle drag location update event
     public func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
-        channel.invokeMethod("updated", arguments: nil)
+        channel?.invokeMethod("updated", arguments: nil)
         return UIDropProposal(operation: .copy)
     }
 
     // Handle drag exit event
     public func dropInteraction(_ interaction: UIDropInteraction, sessionDidExit session: UIDropSession) {
-        channel.invokeMethod("exited", arguments: nil)
+        channel?.invokeMethod("exited", arguments: nil)
     }
 
     // Handle drop event
